afc686d8af00065a82f71f828b32932e9f6805e7
Edvaldo Perreira <edvaldo.mail@gmail.com>
2022-05-22 19:19:15 +0100

    meal data, meal item and styling meals

---
+++ lib/models/meal.dart
@@ -0,0 +1,60 @@
+enum Complexity { simple, medium, difficult }
+
+enum Cost { cheap, fair, expensive }
+
+class Meal {
+  final String id;
+  final List<String> categories;
+  final String title;
+  final Cost cost;
+  final Complexity complexity;
+  final String imageUrl;
+  final int duration;
+  final List<String> ingredients;
+  final List<String> steps;
+  final bool isGlutenFree;
+  final bool isVegan;
+  final bool isVegetarian;
+  final bool isLactoseFree;
+
+  const Meal(
+      {required this.id,
+      required this.categories,
+      required this.title,
+      required this.cost,
+      required this.complexity,
+      required this.imageUrl,
+      required this.duration,
+      required this.ingredients,
+      required this.steps,
+      required this.isGlutenFree,
+      required this.isVegan,
+      required this.isVegetarian,
+      required this.isLactoseFree});
+
+  String get complexityText {
+    switch (complexity) {
+      case Complexity.simple:
+        return "Simple";
+      case Complexity.medium:
+        return "Normal";
+      case Complexity.difficult:
+        return "Difficult";
+      default:
+        return "Unknown";
+    }
+  }
+
+  String get costText {
+    switch (cost) {
+      case Cost.cheap:
+        return "Cheap";
+      case Cost.fair:
+        return "Fair";
+      case Cost.expensive:
+        return "Expensive";
+      default:
+        return "Unknown";
+    }
+  }
+}
--- lib/pages/categories_meals_page.dart
+++ lib/pages/categories_meals_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:meals/components/meal_item.dart';
+import 'package:meals/data/dummy_data.dart';
 import 'package:meals/models/category.dart';
 
 class CategoriesMealsPage extends StatelessWidget {
@@ -7,15 +9,19 @@ class CategoriesMealsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final category = ModalRoute.of(context)!.settings.arguments as Category;
+    final categoryMeals = dummyMeals.where((meal) {
+      return meal.categories.contains(category.id);
+    }).toList();
 
     return Scaffold(
       appBar: AppBar(
         title: Text("${category.title}'s recipes"),
       ),
-      body: Center(
-          child: Text(
-        'Recipes by category - ${category.title}',
-      )),
+      body: ListView.builder(
+          itemCount: categoryMeals.length,
+          itemBuilder: (ctx, index) {
+            return MealItem(categoryMeals[index]);
+          }),
     );
   }
 }
